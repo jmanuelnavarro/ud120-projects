9767e3055390f401d0198d10481e456f5adf81ae
Unknown <jmanuel.navarro@gmail.com>
2017-12-02 11:38:58 +0100

    Mini Project Naive Bayes
    Solution to Naive Bayes mini project. Training and testing using Naive Bayes algorithm and calculating performance and accuracy

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -15,17 +15,34 @@ from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
-
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
-
 #########################################################
 ### your code goes here ###
+from sklearn.naive_bayes import GaussianNB
+
+
+# clf = classifier
+clf = GaussianNB()
+# classifier training
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+# classifier training
+t0 = time()
+clf.predict(features_test)
+print "predicting time:", round(time()-t0, 3), "s"
+
+# classifier testing
+t0 = time()
+accuracy = clf.score(features_test, labels_test)
+print "testing time:", round(time()-t0, 3), "s"
+print "accuracy: ",accuracy
 
 
 #########################################################
