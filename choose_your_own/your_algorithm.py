1be1b7937bb9102fc4211d04f67417e2bc2dbe6a
Unknown <jmanuel.navarro@gmail.com>
2017-12-09 08:37:39 +0100

    Mini Project Choose Your Own Algorithm
    Coding "choose your own algorithm" lesson, playing with K Nearest Neighbors, AdaBoost and Random Forest algorithm

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -30,15 +30,74 @@ plt.show()
 
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
+from sklearn.neighbors import KNeighborsClassifier 
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from time import time
+
+##1.- Using K-Nearest Neighbours
+# Parameters
+algorithmParamArray=['auto','ball_tree','kd_tree','brute']
+
+for algorithmParam in algorithmParamArray:
+    # Train Data
+    print "*******Algortihm used:", algorithmParam
+    print "----------Training Phase (KNeighbors)---------"
+    t0 = time()
+    clfKNN=KNeighborsClassifier(n_neighbors=10,algorithm=algorithmParam)
+    clfKNN.fit(features_train, labels_train)
+    print "training time (KNeighbors):", round(time()-t0, 3), "s"
+    
+    # Test Data
+    print "----------Testing Phase (KNeighbors)---------"
+    accuracy=clfKNN.score(features_test,labels_test)
+    print "Accuracy (KNeighbors):", accuracy, '\n'
+    try:
+        prettyPicture(clfKNN, features_test, labels_test)
+    except NameError:
+        pass
 
 
+##2.- Using Random Forest
+# Parameters
+#algorithmParamArray=['auto','ball_tree','kd_tree','brute']
+algorithmParam='Random Forest' 
+# Train Data
+print "*******Algortihm used:", algorithmParam
+print "----------Training Phase (Random Forest)----"
+t0 = time()
+clfRF=RandomForestClassifier(n_estimators=10)
+clfRF.fit(features_train, labels_train)
+print "training time (Random Forest):", round(time()-t0, 3), "s"
 
-
-
-
-
+# Test Data
+print "----------Testing Phase (Random Forest)----"
+accuracy=clfRF.score(features_test,labels_test)
+print "Accuracy (Random Forest):", accuracy, '\n'
 
 try:
-    prettyPicture(clf, features_test, labels_test)
+    prettyPicture(clfRF, features_test, labels_test)
+except NameError:
+    pass
+
+##3.- Using AdaBoost (Decision Tree as Base Learner)
+# Parameters
+#algorithmParamArray=['auto','ball_tree','kd_tree','brute']
+n_estimators_par=100 
+# Train Data
+print "*******Algortihm used:", " AdaBoost (Tree)"
+print "----------Training Phase (AdaBoost)----"
+t0 = time()
+clfAB=AdaBoostClassifier(n_estimators=n_estimators_par)
+clfAB.fit(features_train, labels_train)
+print "training time (AdaBoost):", round(time()-t0, 3), "s"
+
+# Test Data
+print "----------Testing Phase (Random Forest)----"
+accuracy=clfAB.score(features_test,labels_test)
+print "Accuracy (AdaBoost):", accuracy, '\n'
+
+try:
+    prettyPicture(clfAB, features_test, labels_test)
 except NameError:
     pass
